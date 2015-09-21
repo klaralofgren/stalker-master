04b91ceb7dcffcc890161fcd0a263f51550884b5
klaralofgren <Klara@wlanguest136107.eduroam.hj.se>
2015-09-21 12:49:46 +0200

    start

---
+++ js/js.js
@@ -0,0 +1,19 @@
+function initMap(myLatLng){
+	
+	// Create a map object and specify the DOM element for display.
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: myLatLng,
+		scrollwheel: false,
+		zoom: 8
+	});
+}
+
+function setMarker(myLatLng){
+	// Create a marker and set its position.
+	var marker = new google.maps.Marker({
+		map: map,
+		position: myLatLng,
+		title: 'Hello World!'
+	});
+}
+  
